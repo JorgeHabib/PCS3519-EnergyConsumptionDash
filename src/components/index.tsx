d72767f3fa06e475b068d7909e6072a21a8abf59
JorgeHabib <jorgehabib.khouri@gmail.com>
2023-04-10 22:46:41 -0300

    fix: fixed build script

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,3 @@
-import { Can } from './Can'
 import { FlexButton } from './FlexButton'
 import { Flex } from './Flex'
 import { Box } from './Box'
@@ -6,6 +5,5 @@ import { Box } from './Box'
 export {
   FlexButton,
   Flex,
-  Can,
   Box
 }
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,6 +1,5 @@
-import { useCan } from './useCan'
 import useIsMounted from './useIsMounted'
 import { useOutsideAlerter } from './useOutsideAlerter'
 import { useSkipper } from './useSkipper'
 
-export { useCan, useIsMounted, useOutsideAlerter, useSkipper }
+export { useIsMounted, useOutsideAlerter, useSkipper }
